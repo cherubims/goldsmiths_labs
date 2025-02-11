a730c770a32c3d6ed169a9c3ca6a25087b03e03a
Ruth Efrain <ruthefrain@dc4.com>
2025-02-11 18:14:11 +0300

    Update startscript with Gunicorn and Daphne background separate processes

--- start.sh
+++ start.sh
@@ -2,3 +2,6 @@
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn goldsmiths_labs.wsgi:application
+
+gunicorn goldsmiths_labs.wsgi:application --bind 0.0.0.0:8000 &   # Run Gunicorn in the background
+daphne -b 0.0.0.0 -p 8001 goldsmiths_labs.asgi:application         # Run Daphne on a separate port
